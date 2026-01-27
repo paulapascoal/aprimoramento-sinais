5ee55d5c523c6d9e0460b67d2d5de13d1e0d698d
Paula Pascoal <paulaiasmimpascoal2012@gmail.com>
2026-01-27 08:53:55 -0300

    Update script.js
    refatorado

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function mudouTamanho(){
-            if (window.innerWidth >= 768 ){
-                itens.style.display='block'
+const menu = document.getElementById('itens');
 
-            }else {
-                itens.style.display = 'none'
-            }
-        }
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
 
-        function clickMenu(){
-            if (itens.style.display =='block'){
-                itens.style.display ='none'
-            } else{
-                itens.style.display ='block'
-            }
-            
-        }
+function clickMenu() {
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+window.addEventListener('load', mudouTamanho);
+window.addEventListener('resize', mudouTamanho);
